5d66303961d51662f2a8522691e43d5f5c7859b1
num <s000032001@gmail.com>
2015-03-05 20:23:47 +0800

    remove comment

--- q10041.cpp
+++ q10041.cpp
@@ -1,30 +1,6 @@
 /*
-
-https://hackpad.com/UVa-Q10041-g0kdUs0MGof
-
-Q10041: Vito's family
-
-世界聞名的黑社會老大Vito Deadstone要搬到紐約來了。
-在那裡他有一個大家族，並且他們都住在Lamafia大道上。
-因為Vito時常要拜訪所有的親戚，他想要找一間離他們最近的房子，也就是說他希望從他的家到所有的親戚的家的距離的和為最小。
-
-他恐嚇你寫一個程式來幫助幫助他解決這個問題。
-
-Input
-
-輸入的第一列有一個整數代表以下有多少組測試資料。
-
-每組測試資料一列，第一個整數 r（0 < r < 500），代表他親戚的數目。
-接下來的r個整數s1,s2,......sr為這些親戚房子的門牌號碼（0 < si <30000）。
-注意：有些親戚的門牌號碼會相同。
-
-Output
-
-對每一組測試資料，輸出從他的新家到所有的親戚的家的距離的和為最小為多少。2個門牌號碼si、sj的距離為si-sj的絕對值。
-
-http://www3.tcgs.tc.edu.tw/~sagit/luckycat/q10041.htm
-
-*/
+ * https://hackpad.com/UVa-Q10041-g0kdUs0MGof
+ */
 
 #include <stdio.h>
 #include <assert.h>
